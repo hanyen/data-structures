f739d6d1daeb641608c3aad70766e45655516ac1
hanyen <me@hanyen.com>
2016-12-22 06:53:43 -0800

    Add line-by-line pseudocodes

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -16,25 +16,36 @@ var treeMethods = {};
 //takes any value, sets that as the target of a node, and adds that 
 //node as a child of the tree
 treeMethods.addChild = function(value) {
+  //call the Tree function to create an object and push it to .children array
   this.children.push(Tree(value));
 };
 
 // takes any input and returns a boolean reflecting whether it can 
 // be found as the value of the target node or any descendant node
 treeMethods.contains = function(target) {
+
+  //declare an accumulator variable and set it to false
   var acc = false;
+  //create a traverse function that accept a children array
   var traverse = function (childrenArray) {  
+    //iterate through the children array
     for (var i = 0; i < childrenArray.length; i++) {
+      //check if the current element is equal the passed argument (target)
       if (childrenArray[i].value === target) {
+        // if yes, set to true 
         acc = true;
         // check if children array contains value
-      } else if (childrenArray[i].children.length > 0 ) {
-        // it goes here because it couldn't find the target in the first level
-        traverse (childrenArray[i].children);
+      } 
+        // else, check if the current element's children array holds value (i.e. has children)
+        else if (childrenArray[i].children.length > 0 ) {
+          // if yes, call traverse() again and pass the element's children array
+          traverse (childrenArray[i].children);
       } 
     }
+    // When the base case is reached (no more children, i.e. children array has no value) return the accumulator.
     return acc;
   };
+  // call the traverse function and pass the root's children array
   return traverse(this.children);
 };
 
