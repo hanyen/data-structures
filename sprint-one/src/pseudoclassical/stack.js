1d918e0e4b0197bacb22f10d654bd9f467838716
hanyen <me@hanyen.com>
2016-12-28 11:17:44 -0800

    Added pseudocode

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,24 +3,30 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   this.sizeVar = 0;
   this.storage = {};
-
 };
 
 Stack.prototype.size = function() {
   return this.sizeVar;
-};
+}
 
 Stack.prototype.push = function(value) {
+  // increment sizeVar
   this.sizeVar++;
+  // create a property with sizeVar as the key and the argument as the value
   this.storage[this.sizeVar] = value;
-};
+}
 
 Stack.prototype.pop = function() {
+  // create a variable to hold the value to be returned
   var returnValue = this.storage[this.sizeVar];
+  // delete the property to be popped
   delete this.storage[this.sizeVar];
+  // decrease sizeVar
   this.sizeVar--;
-  if (this.sizeVar < 1) {
+  // check if sizeVar is less than 1, set it to 0
+  if(this.sizeVar < 1) {
     this.sizeVar = 0;
   }
+  // return the popped value
   return returnValue;
-};
+}
