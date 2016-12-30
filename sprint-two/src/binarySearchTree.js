eb7fb5222ecd0ceba7e91bcb66573d1a2b979fac
hanyen <me@hanyen.com>
2016-12-29 19:26:55 -0800

    pass the .contains method test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,6 @@
 var BinarySearchTree = function(value) {
   var binaryTree = Object.create(treePrototype);
-  binaryTree.value = 5;
+  binaryTree.value = 5; //set initial value
   binaryTree.left = null;
   binaryTree.right = null;
   return binaryTree;
@@ -14,7 +14,9 @@ treePrototype.insert = function(item) {
   var node = Object.create(treePrototype);
   //set the new object's value to the argument
   node.value = item;
-    
+  if (this.value === item) {
+    //do nothing
+  }
   //if the current node's value > the argument
   if (this.value > item) {
     //need to add as leaf
@@ -25,8 +27,8 @@ treePrototype.insert = function(item) {
     } else {
       this.left.right = node;
     }
-    
-  } else {
+  //if current node's value < the argument
+  } else if (this.value < item) {
     //need to add as leaf
     if (this.right === null) {
       this.right = node;  
@@ -40,8 +42,44 @@ treePrototype.insert = function(item) {
   console.log(this);
 };
 
-treePrototype.contains = function(item) {
+treePrototype.contains = function(target) {
+  console.log('-------');
+  var found = false;
   
+  var traverseLeft = function(node) {
+    
+    //if found in root
+    if (node.value === target) {
+      found = true;
+    }
+
+    //traverse left first
+    if (node.left) {
+      if (node.left.value === target) {
+      found = true;
+      } else {
+        traverseLeft(node.left);
+        traverseRight(node.left);
+      }
+    }  
+  }
+
+  var traverseRight = function(node) {
+    //traverse right first
+    if (node.right) {
+      if (node.right.value === target) {
+        found = true;
+      } else {
+        traverseLeft(node.right);
+        traverseRight(node.right);
+      }
+    }  
+  }
+
+  traverseLeft(this);
+  traverseRight(this);
+  
+  return found;
 };
 
 treePrototype.depthFirstLog = function(item) {
